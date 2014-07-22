53fa8f5f13ecb4565113b987e9dd5568bc92bd79
ap4y <lod@pisem.net>
2014-07-22 18:20:44 +1200

    Migrate swift bindings to the beta 4

--- LevelDB/LevelDB.swift
+++ LevelDB/LevelDB.swift
@@ -8,38 +8,38 @@
 
 import Foundation
 
-let defaultWriteOptions = WriteOptions(sync: false);
-let defaultReadOptions  = ReadOptions(verifyChecksums: false, fillCache: true);
+public let defaultWriteOptions = WriteOptions(sync: false);
+public let defaultReadOptions  = ReadOptions(verifyChecksums: false, fillCache: true);
 
 extension LevelDB: Sequence {
 
-    func putKey(key: String, value: String, error: NSErrorPointer) -> Bool {
+    public func putKey(key: String, value: String, error: NSErrorPointer) -> Bool {
         return putKey(key, value: value, options: defaultWriteOptions, error: error);
     }
 
-    func getKey(key: String, error: NSErrorPointer) -> String {
+    public func getKey(key: String, error: NSErrorPointer) -> String? {
         return getKey(key, options: defaultReadOptions, error: error);
     }
 
-    func deleteKey(key: String, error: NSErrorPointer) -> Bool {
+    public func deleteKey(key: String, error: NSErrorPointer) -> Bool {
         return deleteKey(key, options: defaultWriteOptions, error: error);
     }
 
-    func writeBatch(operations: ((WriteBatch!) -> Void)) -> NSError? {
+    public func writeBatch(operations: ((WriteBatch!) -> Void)) -> NSError? {
         var error: NSError?
         let result = write(operations, options: defaultWriteOptions, error: &error)
 
         return result ? nil : error
     }
 
-    func generate() -> DatabaseEnumerator {
+    public func generate() -> DatabaseEnumerator {
         return enumeratorWithOptions(defaultReadOptions)
     }
 }
 
 extension DatabaseEnumerator: Generator {
 
-    func next() -> (String, String)? {
+    public func next() -> (String, String)? {
         if let dict = nextObject() as? [String: String] {
             return (dict[LevelDBEnumeratorResultKey]!, dict[LevelDBEnumeratorResultValue]!)
         }
@@ -47,14 +47,14 @@ extension DatabaseEnumerator: Generator {
         return nil;
     }
 
-    func allValuesUntilPrefix(prefix: String) -> [(String, String)] {
+    public func allValuesUntilPrefix(prefix: String) -> [(String, String)] {
         let result = allObjectsUntilPrefix(prefix) as [[String: String]]
         return result.map {
             ($0[LevelDBEnumeratorResultKey]!, $0[LevelDBEnumeratorResultValue]!)
         }
     }
 
-    func allValuesWithPrefix(prefix: String) -> [(String, String)] {
+    public func allValuesWithPrefix(prefix: String) -> [(String, String)] {
         let result = allObjectsWithPrefix(prefix) as [[String: String]]
         return result.map {
             ($0[LevelDBEnumeratorResultKey]!, $0[LevelDBEnumeratorResultValue]!)
